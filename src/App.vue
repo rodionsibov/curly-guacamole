a7f6c69b58b2a55c764ec2ca8423d545cad298b1
Rodion Sibov <rodionsibov@gmail.com>
2022-02-28 23:00:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+
+import { ref } from "@vue/reactivity";
+
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+const loadUserData = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: "Name Eman",
+        pic: null,
+        bio: null,
+      });
+    }, 2000);
+  });
+};
+
+const userData = ref(await loadUserData());
+</script>
+
+<template>
+  <div class="profile-card">
+    <div class="profile-image">
+      <img :src="userData.pic" alt="">
+    </div>
+    <div class="profile-info">
+      <span>Written By</span>
+      <h3>
+        {{userData.name}}
+      </h3>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
