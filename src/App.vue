ae554bbcd4d5a07d4ca5b6a1f0d6e045c37e9d62
Rodion Sibov <rodionsibov@gmail.com>
2022-03-03 11:33:24 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,16 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
-
-import { ref } from "@vue/reactivity";
-import { computed, onMounted } from "@vue/runtime-core";
-
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-const loadUserData = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        name: "Name",
-        pic: 'https://picsum.photos/300',
-        bio: 'Bio',
-      });
-    }, 2000);
-  });
-};
 
-const userData = ref(null);
-
-onMounted(async () => {
-  userData.value = await loadUserData();
-});
+import ProfileCard from "./components/ProfileCard.vue";
 </script>
 
 <template>
   <Suspense>
     <template #default>
-      <div class="profile-card" v-if="userData">
-        <div class="profile-image">
-          <img :src="userData.pic" alt="" />
-          <h3>
-            {{ userData.name }}
-          </h3>
-          <p>
-            {{ userData.bio }}
-          </p>
-        </div>
-      </div>
+      <ProfileCard />
     </template>
-    <template #fallback>Loading...</template>
+    <template #fallback> Loading... </template>
   </Suspense>
 </template>
 
