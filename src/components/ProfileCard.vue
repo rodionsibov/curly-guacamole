0073fff4b39665b4ee617984d0065a96121caf04
Rodion Sibov <rodionsibov@gmail.com>
2022-03-03 11:33:40 +0200

    first commit

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "@vue/reactivity";
+import { computed, onMounted } from "@vue/runtime-core";
+
+const loadUserData = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: "Name",
+        pic: "https://picsum.photos/150",
+        bio: "Bio",
+      });
+    }, 4000);
+  });
+};
+
+const userData = await loadUserData();
+
+</script>
+
+<template>
+  <div class="profile-card" v-if="userData">
+    <div class="profile-image">
+      <img :src="userData.pic" alt="" />
+      <h3>
+        {{ userData.name }}
+      </h3>
+      <p>
+        {{ userData.bio }}
+      </p>
+    </div>
+  </div>
+</template>
